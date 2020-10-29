3ed789ec27f475189c5117507d929a244762538e
Julia <juliet_v17@mail.ru>
2020-10-29 16:41:26 +0300

    Update AbstractDao.java

--- src/main/java/web/dao/AbstractDao.java
+++ src/main/java/web/dao/AbstractDao.java
@@ -5,15 +5,9 @@ import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 
-public abstract class AbstractDao<PK extends Serializable, T> {
+public abstract class AbstractDao {
 
-    private final Class<T> persistentClass;
-
-    @SuppressWarnings("unchecked")
-    public AbstractDao(){
-        this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
-    }
-
+    
     @PersistenceContext
     EntityManager entityManager;
 
@@ -21,20 +15,5 @@ public abstract class AbstractDao<PK extends Serializable, T> {
         return this.entityManager;
     }
 
-    protected T getByKey(PK key) {
-        return (T) entityManager.find(persistentClass, key);
-    }
-
-    protected void persist(T entity) {
-        entityManager.persist(entity);
-    }
-
-    protected void update(T entity) {
-        entityManager.merge(entity);
-    }
-
-    protected void delete(T entity) {
-        entityManager.remove(entity);
-    }
 
 }
